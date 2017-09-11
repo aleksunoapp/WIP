33afccc89ed4f8e175eb10dd1f1c3f4d2cd40f28
Aleks <aleks@unoapp.com>
2017-09-11 15:18:11 -0400

    Added MongoDB login details (for logging user activity)

--- src/environment.js
+++ src/environment.js
@@ -1,3 +1,6 @@
 module.exports = {
-	production_url: 'https://test2dynamicmpiapi.dealer-fx.com'
+	production_url: 'https://test2dynamicmpiapi.dealer-fx.com',
+	mongodb_url: '172.31.51.105',
+	mongodb_username: 'admin',
+	mongodb_password: '@ITSupport1'
 }
