83f38b38834d1b9d887ee411b9b15f67603dc46c
Aaron Tucker <aaron@unoapp.com>
2018-01-03 12:24:45 -0500

    Set up the defer modal

---
+++ src/components/DeferModal.vue
@@ -0,0 +1,35 @@
+<template>
+	<div class="modal">
+		<div class="modal-window">
+			<div class="modal-content">
+				<div class="modal-header">
+					Select a reason for deferring this service
+				</div>
+				<div class="modal-message no-padding-bottom no-padding-top">
+					<ul class="modal-list-options">
+						<template v-for="reason in $root.meta.declinedReasons">
+							<li @click="emitReason(reason)">{{ reason.name }}</li>
+						</template>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+<script>
+export default {
+	name: 'defer-modal',
+	props: {},
+	methods: {
+		/**
+		 * To tell the parent component to the defer reason
+		 * @function
+		 * @param {object} reason - The reason for deferral
+		 * @returns {undefined}
+		 */
+		emitReason (reason) {
+			this.$emit('deferReason', reason)
+		}
+	}
+}
+</script>
