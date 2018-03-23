cf4ff8abf6ae39917a35dcd071edf07dacecd833
Amandeep Singh <amandeep@unoapp.com>
2018-03-23 14:53:20 -0400

    Fixed env for DFX.

--- src/environment.js
+++ src/environment.js
@@ -1,4 +1,4 @@
 module.exports = {
-	production_url: 'https://test2dynamicmpiapi.dealer-fx.com/',
+	production_url: '#{ApiUrl}#',
 	userActivityLogAPI: 'https://test2dynamicmpiapi.dealer-fx.com/'
 }
