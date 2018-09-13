119f8cdc45890acc355643318b0a868ed5fe19fc
Aleks Sobieraj <aleks@unoapp.com>
2018-09-13 17:09:08 -0400

    Change resources to beta api

--- src/global.js
+++ src/global.js
@@ -95,7 +95,7 @@ export default {
 	 */
 	resourcesBaseUrl: (function () {
 		if (production) {
-			return 'https://resources.api.unoapp.io'
+			return 'https://resources.beta.api.unoapp.io'
 		} else if (staging) {
 			return 'https://resources.dev.api.unoapp.io'
 		} else if (development) {
