46b5dbe41989e27bd6f4442ace2cd340a4c41a8c
Aleks Sobieraj <aleks@unoapp.com>
2018-11-29 11:43:28 -0500

    EE: Add pita-pit to list of hostnames in config

--- src/global.js
+++ src/global.js
@@ -4,6 +4,7 @@ import laprepConfig from '@/global_LAPREP.js'
 
 const freshii = 'freshii'
 const pitapit = 'pitapit'
+const pitaDashPit = 'pita-pit'
 const laprep = 'laprep'
 let client = window.location.hostname.split('.')[0]
 
@@ -15,6 +16,8 @@ const fileRouter = (function () {
 	if (client === freshii) {
 		return freshiiConfig
 	} else if (client === pitapit) {
+		return pitapitConfig
+	} else if (client === pitaDashPit) {
 		return pitapitConfig
 	} else if (client === laprep) {
 		return laprepConfig
