89b15f72109a1b773eccb6e75aee5c724728dd8c
Aleks Sobieraj <aleks@unoapp.com>
2019-01-25 14:57:38 -0500

    Hide promise if no customer concerns

--- src/components/Services.vue
+++ src/components/Services.vue
@@ -144,7 +144,7 @@
 					<span v-if="!timeExpired">{{ langTerms.if_approved_by[$root.meta.local.toLowerCase()] }} {{ formatTime(this.$root.meta.responseBy) }} {{ langTerms.your_vehicle_will_be_ready[$root.meta.local.toLowerCase()] }} {{ formatTime(this.$root.meta.promise) }}.</span>
 					<span v-else>{{ langTerms.your_service_advisor_will[$root.meta.local.toLowerCase()] }}</span>
 				</div>
-				<div v-if="!$root.inspectionCounts.failCount && !$root.inspectionCounts.warningCount" class="time-notice">
+				<div v-if="!$root.inspectionCounts.failCount && !$root.inspectionCounts.warningCount && !$root.inspectionCounts.concernCount" class="time-notice">
 					<span>{{ langTerms.your_vehicle_will_be_ready[$root.meta.local.toLowerCase()] }} {{ formatTime() }}.</span>
 				</div>
 				<div class="summary-table">
