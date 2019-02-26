90dcf904a1b1ae602c85d9a03f575e2248ede503
Aleks Sobieraj <aleks@unoapp.com>
2019-02-26 15:50:07 -0500

    Center Terms and Conditions

--- src/views/TermsAndConditions.vue
+++ src/views/TermsAndConditions.vue
@@ -1,25 +1,27 @@
 <template>
-  <div class="contents">
-    <h1 class="header">
-      {{ $t("terms_and_conditions") }}
-    </h1>
-    <p>
-      {{ $t("paragraph1") }}
-    </p>
-    <p>
-      {{ $t("paragraph2") }}
-    </p>
-    <div
-      class="button cta green"
-      @click="accept()"
-    >
-      {{ $t("accept") }}
-    </div>
-    <div
-      class="button skip"
-      @click="decline()"
-    >
-      {{ $t("decline") }}
+  <div class="container">
+    <div class="contents">
+      <h1 class="header">
+        {{ $t("terms_and_conditions") }}
+      </h1>
+      <p>
+        {{ $t("paragraph1") }}
+      </p>
+      <p>
+        {{ $t("paragraph2") }}
+      </p>
+      <div
+        class="button cta green"
+        @click="accept()"
+      >
+        {{ $t("accept") }}
+      </div>
+      <div
+        class="button skip"
+        @click="decline()"
+      >
+        {{ $t("decline") }}
+      </div>
     </div>
   </div>
 </template>
@@ -55,43 +57,47 @@ export default Vue.extend({
 </script>
 
 <style lang="scss" scoped>
-.contents {
-  max-width: 768px;
+.container {
   display: flex;
-  flex-direction: column;
   justify-content: center;
-  align-items: center;
-  padding: 1rem;
-  .header {
-    text-align: center;
-  }
-  .button {
-    width: 100%;
-    margin: 1rem 0;
-    padding: 0;
-    text-transform: uppercase;
-    text-align: center;
-    border: none;
-  }
-  .button.cta {
+  .contents {
+    max-width: 768px;
     display: flex;
+    flex-direction: column;
     justify-content: center;
     align-items: center;
-    font-family: 'Futura Heavy';
-    height: 50px;
-    line-height: 2rem;
-    font-size: 1.8rem;
-    font-weight: 400;
-    box-shadow: var(--shadow);
-    background-color: var(--blue);
-    color: var(--white);
-    border-radius: 3px;
-  }
-  .button.cta.green {
-    background-color: var(--green);
-  }
-  .button.skip {
-    color: var(--black);
+    padding: 1rem;
+    .header {
+      text-align: center;
+    }
+    .button {
+      width: 100%;
+      margin: 1rem 0;
+      padding: 0;
+      text-transform: uppercase;
+      text-align: center;
+      border: none;
+    }
+    .button.cta {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      font-family: 'Futura Heavy';
+      height: 50px;
+      line-height: 2rem;
+      font-size: 1.8rem;
+      font-weight: 400;
+      box-shadow: var(--shadow);
+      background-color: var(--blue);
+      color: var(--white);
+      border-radius: 3px;
+    }
+    .button.cta.green {
+      background-color: var(--green);
+    }
+    .button.skip {
+      color: var(--black);
+    }
   }
 }
 </style>
