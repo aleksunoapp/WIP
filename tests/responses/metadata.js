7d35da6b13b410a938823759e680affe9a0d6e5c
Aleks Sobieraj <aleks@unoapp.com>
2019-04-12 11:07:37 -0400

    Export sample responses

---
+++ tests/responses/metadata.js
@@ -0,0 +1,378 @@
+const enCA = {
+  'local': 'en-CA',
+  'firstName': 'Jo',
+  'lastName': 'Smith',
+  'topImageUrl': 'http://unoapp.com/dfx-mpi/v1/images/nissan-logo.png',
+  'bottomImageUrl': 'http://unoapp.com/dfx-mpi/v1/images/nissan-car.png',
+  'disclaimer': 'The information contained on this Website (the "Service") is for general information purposes only.Dealer - Fx assumes no responsibility for errors or omissions in the contents on the Service.',
+  'carDescription': '2010 ALTIMA',
+  'carImageUrl': 'http://unoapp.com/dfx-mpi/v1/images/nissan-car.png',
+  'themeId': 'NISSAN',
+  'promise': '2019-05-30T23:30:00+00:00',
+  'responseBy': '2019-03-30T23:00:00+00:00',
+  'status': '5',
+  'expired': false,
+  'isBusiness': false,
+  'serviceCategories': [
+    {
+      'id': '1',
+      'name': 'Safety Concerns',
+      'serviceCategoryType': 'SAFETY',
+      'iconUrl': 'http://www.unoapp.com/dfx-mpi/v1/images/fail-white.png',
+      'sortOrder': 1,
+      'defaultExpended': true,
+      'isPreApproved': false,
+      'showOnSummary': true,
+      'showOnInspection': true
+    },
+    {
+      'id': '2',
+      'name': 'Items Requiring Attention',
+      'serviceCategoryType': 'ATTN',
+      'iconUrl': 'http://www.unoapp.com/dfx-mpi/v1/images/warning-white.png',
+      'sortOrder': 2,
+      'defaultExpended': true,
+      'isPreApproved': false,
+      'showOnSummary': true,
+      'showOnInspection': true
+    },
+    {
+      'id': '3',
+      'name': 'Items Not Requiring Any Action',
+      'serviceCategoryType': 'PASS',
+      'iconUrl': 'http://unoapp.com/dfx-mpi/v1/images/pass-white.png',
+      'sortOrder': 3,
+      'defaultExpended': false,
+      'isPreApproved': false,
+      'showOnSummary': false,
+      'showOnInspection': true
+    },
+    {
+      'id': '4',
+      'name': 'Previously Approved Services',
+      'serviceCategoryType': 'PRE',
+      'iconUrl': null,
+      'sortOrder': 99,
+      'defaultExpended': true,
+      'isPreApproved': true,
+      'showOnSummary': true,
+      'showOnInspection': false
+    },
+    {
+      'id': '5',
+      'name': 'Customer Concerns',
+      'serviceCategoryType': 'CC',
+      'iconUrl': 'https://test2dynamicmpibridge.dealer-fx.com/Content/images/ServiceCategory/Guest_Concerns_Greybard.png',
+      'sortOrder': 4,
+      'defaultExpended': true,
+      'isPreApproved': false,
+      'showOnSummary': true,
+      'showOnInspection': false
+    }
+  ],
+  'dealerContactInfo': {
+    'name': 'Nissan of Anytown USA',
+    'logoUrl': null,
+    'phone': '7709988686',
+    'email': 'service@regalauto.com',
+    'address': '123 Demo Street',
+    'smsPhone': '12242035286'
+  },
+  'declinedReasons': [
+    {
+      'id': 1,
+      'name': "I don't have time"
+    },
+    {
+      'id': 2,
+      'name': 'I am concerned with the cost'
+    },
+    {
+      'id': 3,
+      'name': 'I cannot afford this item'
+    },
+    {
+      'id': 4,
+      'name': 'I am considering changing my vehicle'
+    },
+    {
+      'id': 5,
+      'name': 'I need alternate transportation'
+    },
+    {
+      'id': 0,
+      'name': 'No reason'
+    }
+  ],
+  'supportedLanguages': [
+    {
+      'culture': 'en-CA',
+      'name': 'English'
+    },
+    {
+      'culture': 'fr-CA',
+      'name': 'French'
+    },
+    {
+      'culture': 'es-mx',
+      'name': 'Spanish'
+    }
+  ],
+  'customerConcernsLabel': 'Customer Concerns',
+  'customerCommentsLabel': 'Customer Comments'
+}
+
+const frCA = {
+  'local': 'en-CA',
+  'firstName': 'Jo',
+  'lastName': 'Smith',
+  'topImageUrl': 'http://unoapp.com/dfx-mpi/v1/images/nissan-logo.png',
+  'bottomImageUrl': 'http://unoapp.com/dfx-mpi/v1/images/nissan-car.png',
+  'disclaimer': 'The information contained on this Website (the "Service") is for general information purposes only.Dealer - Fx assumes no responsibility for errors or omissions in the contents on the Service.',
+  'carDescription': '2010 ALTIMA',
+  'carImageUrl': 'http://unoapp.com/dfx-mpi/v1/images/nissan-car.png',
+  'themeId': 'NISSAN',
+  'promise': '2019-05-30T23:30:00+00:00',
+  'responseBy': '2019-03-30T23:00:00+00:00',
+  'status': '5',
+  'expired': false,
+  'isBusiness': false,
+  'serviceCategories': [
+    {
+      'id': '1',
+      'name': 'FR-Safety Concerns',
+      'serviceCategoryType': 'SAFETY',
+      'iconUrl': 'http://www.unoapp.com/dfx-mpi/v1/images/fail-white.png',
+      'sortOrder': 1,
+      'defaultExpended': true,
+      'isPreApproved': false,
+      'showOnSummary': true,
+      'showOnInspection': true
+    },
+    {
+      'id': '2',
+      'name': 'FR-Items Requiring Attention',
+      'serviceCategoryType': 'ATTN',
+      'iconUrl': 'http://www.unoapp.com/dfx-mpi/v1/images/warning-white.png',
+      'sortOrder': 2,
+      'defaultExpended': true,
+      'isPreApproved': false,
+      'showOnSummary': true,
+      'showOnInspection': true
+    },
+    {
+      'id': '3',
+      'name': 'FR-Items Not Requiring Any Action',
+      'serviceCategoryType': 'PASS',
+      'iconUrl': 'http://unoapp.com/dfx-mpi/v1/images/pass-white.png',
+      'sortOrder': 3,
+      'defaultExpended': false,
+      'isPreApproved': false,
+      'showOnSummary': false,
+      'showOnInspection': true
+    },
+    {
+      'id': '4',
+      'name': 'FR-Previously Approved Services',
+      'serviceCategoryType': 'PRE',
+      'iconUrl': null,
+      'sortOrder': 99,
+      'defaultExpended': true,
+      'isPreApproved': true,
+      'showOnSummary': true,
+      'showOnInspection': false
+    },
+    {
+      'id': '5',
+      'name': 'FR-Customer Concerns',
+      'serviceCategoryType': 'CC',
+      'iconUrl': 'https://test2dynamicmpibridge.dealer-fx.com/Content/images/ServiceCategory/Guest_Concerns_Greybard.png',
+      'sortOrder': 4,
+      'defaultExpended': true,
+      'isPreApproved': false,
+      'showOnSummary': true,
+      'showOnInspection': false
+    }
+  ],
+  'dealerContactInfo': {
+    'name': 'Nissan of Anytown USA',
+    'logoUrl': null,
+    'phone': '7709988686',
+    'email': 'service@regalauto.com',
+    'address': '123 Demo Street',
+    'smsPhone': '12242035286'
+  },
+  'declinedReasons': [
+    {
+      'id': 1,
+      'name': "FR-I don't have time"
+    },
+    {
+      'id': 2,
+      'name': 'FR-I am concerned with the cost'
+    },
+    {
+      'id': 3,
+      'name': 'FR-I cannot afford this item'
+    },
+    {
+      'id': 4,
+      'name': 'FR-I am considering changing my vehicle'
+    },
+    {
+      'id': 5,
+      'name': 'FR-I need alternate transportation'
+    },
+    {
+      'id': 0,
+      'name': 'FR-No reason'
+    }
+  ],
+  'supportedLanguages': [
+    {
+      'culture': 'en-CA',
+      'name': 'English'
+    },
+    {
+      'culture': 'fr-CA',
+      'name': 'French'
+    },
+    {
+      'culture': 'es-mx',
+      'name': 'Spanish'
+    }
+  ],
+  'customerConcernsLabel': 'Préoccupations du client',
+  'customerCommentsLabel': 'Commentaires du client'
+}
+
+const esMX = {
+  'local': 'en-CA',
+  'firstName': 'Jo',
+  'lastName': 'Smith',
+  'topImageUrl': 'http://unoapp.com/dfx-mpi/v1/images/nissan-logo.png',
+  'bottomImageUrl': 'http://unoapp.com/dfx-mpi/v1/images/nissan-car.png',
+  'disclaimer': 'The information contained on this Website (the "Service") is for general information purposes only.Dealer - Fx assumes no responsibility for errors or omissions in the contents on the Service.',
+  'carDescription': '2010 ALTIMA',
+  'carImageUrl': 'http://unoapp.com/dfx-mpi/v1/images/nissan-car.png',
+  'themeId': 'NISSAN',
+  'promise': '2019-05-30T23:30:00+00:00',
+  'responseBy': '2019-03-30T23:00:00+00:00',
+  'status': '5',
+  'expired': false,
+  'isBusiness': false,
+  'serviceCategories': [
+    {
+      'id': '1',
+      'name': 'ES-Safety Concerns',
+      'serviceCategoryType': 'SAFETY',
+      'iconUrl': 'http://www.unoapp.com/dfx-mpi/v1/images/fail-white.png',
+      'sortOrder': 1,
+      'defaultExpended': true,
+      'isPreApproved': false,
+      'showOnSummary': true,
+      'showOnInspection': true
+    },
+    {
+      'id': '2',
+      'name': 'ES-Items Requiring Attention',
+      'serviceCategoryType': 'ATTN',
+      'iconUrl': 'http://www.unoapp.com/dfx-mpi/v1/images/warning-white.png',
+      'sortOrder': 2,
+      'defaultExpended': true,
+      'isPreApproved': false,
+      'showOnSummary': true,
+      'showOnInspection': true
+    },
+    {
+      'id': '3',
+      'name': 'ES-Items Not Requiring Any Action',
+      'serviceCategoryType': 'PASS',
+      'iconUrl': 'http://unoapp.com/dfx-mpi/v1/images/pass-white.png',
+      'sortOrder': 3,
+      'defaultExpended': false,
+      'isPreApproved': false,
+      'showOnSummary': false,
+      'showOnInspection': true
+    },
+    {
+      'id': '4',
+      'name': 'ES-Previously Approved Services',
+      'serviceCategoryType': 'PRE',
+      'iconUrl': null,
+      'sortOrder': 99,
+      'defaultExpended': true,
+      'isPreApproved': true,
+      'showOnSummary': true,
+      'showOnInspection': false
+    },
+    {
+      'id': '5',
+      'name': 'ES-Customer Concerns',
+      'serviceCategoryType': 'CC',
+      'iconUrl': 'https://test2dynamicmpibridge.dealer-fx.com/Content/images/ServiceCategory/Guest_Concerns_Greybard.png',
+      'sortOrder': 4,
+      'defaultExpended': true,
+      'isPreApproved': false,
+      'showOnSummary': true,
+      'showOnInspection': false
+    }
+  ],
+  'dealerContactInfo': {
+    'name': 'Nissan of Anytown USA',
+    'logoUrl': null,
+    'phone': '7709988686',
+    'email': 'service@regalauto.com',
+    'address': '123 Demo Street',
+    'smsPhone': '12242035286'
+  },
+  'declinedReasons': [
+    {
+      'id': 1,
+      'name': "ES-I don't have time"
+    },
+    {
+      'id': 2,
+      'name': 'ES-I am concerned with the cost'
+    },
+    {
+      'id': 3,
+      'name': 'ES-I cannot afford this item'
+    },
+    {
+      'id': 4,
+      'name': 'ES-I am considering changing my vehicle'
+    },
+    {
+      'id': 5,
+      'name': 'ES-I need alternate transportation'
+    },
+    {
+      'id': 0,
+      'name': 'ES-No reason'
+    }
+  ],
+  'supportedLanguages': [
+    {
+      'culture': 'en-CA',
+      'name': 'English'
+    },
+    {
+      'culture': 'fr-CA',
+      'name': 'French'
+    },
+    {
+      'culture': 'es-mx',
+      'name': 'Spanish'
+    }
+  ],
+  'customerConcernsLabel': 'ES-Customer Concerns',
+  'customerCommentsLabel': 'ES-Customer Comments'
+}
+
+export default {
+  'en-CA': enCA,
+  'en-US': enCA,
+  'fr-CA': frCA,
+  'es-MX': esMX
+}
